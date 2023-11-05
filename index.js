354c8cc01b2b5c1e32a77b4cfcc95bb50316710f
alemranakash <alemranexpert@gmail.com>
2023-11-05 09:21:44 +0600

    Added a POST method to store data in MongoDB

--- index.js
+++ index.js
@@ -28,13 +28,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const blogCollection = client.db('blogDB').collection('allBlogs');
+
+// * all blog
+// * add new blog
+    app.post('/allBlogs', async (req, res) => {
+      const addBlogs = req.body;
+      console.log('addBlogs', addBlogs)
+      const result = await blogCollection.insertOne(addBlogs);
+      res.send(result);
+    })
+// *show all blog
+app.get('/allBlogs', async (req, res) => {
+  const result = await blogCollection.find().toArray();
+  res.send(result)
+})
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
