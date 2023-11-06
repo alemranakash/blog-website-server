bf4447f6c4b0fbab7df728f1b4b6a306eb9b8ca7
alemranakash <alemranexpert@gmail.com>
2023-11-06 11:47:18 +0600

    Added secret key for JWT authentication

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function run() {
 app.post('/jwt', async(req, res)=>{
   const user = req.body;
   console.log('User for token' , user);
-  const token = jwt.sign(user, 'secret', 
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, 
   {expiresIn: '1h'})
 })
 
